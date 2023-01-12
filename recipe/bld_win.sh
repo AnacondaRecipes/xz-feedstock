0dca649cbeb8aa3bf08f4657937edfda68a1f236
Eric Lundby <Eric.Lundby@gmail.com>
2023-01-12 00:09:42 +0000

    Update lib paths

--- recipe/bld_win.sh
+++ recipe/bld_win.sh
@@ -19,7 +19,7 @@ make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
 
-cp -v src/liblzma/.libs/liblzma-*.a "$LIBRARY_PREFIX/lib/liblzma-static.lib"
+cp -v src/liblzma/.libs/liblzma.a "$LIBRARY_PREFIX/lib/liblzma_static.lib"
 
 make distclean
 
@@ -37,3 +37,6 @@ make -C src/xz LDFLAGS=-static
 make -C tests check
 
 cp -v src/liblzma/.libs/liblzma-*.dll "$LIBRARY_PREFIX/bin/liblzma.dll"
+cp -v src/liblzma/.libs/liblzma.a "$LIBRARY_PREFIX/lib/liblzma.lib"
+
+find $PREFIX -name '*.la' -delete
