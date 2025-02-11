4e3672ce581c0e943d9b436053cc5a0f336cee08
Eric Lundby <Eric.Lundby@gmail.com>
2025-02-11 14:31:36 -0700

    White space

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ cmake -GNinja ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_C_USE_RESPONSE_FILE_FOR_OBJECTS=FALSE ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DBUILD_SHARED_LIBS=ON ^ 
+      -DBUILD_SHARED_LIBS=ON ^
       ..
 if errorlevel 1 exit /b 1
 
